4272741ac171c4a8b1aa210b9c78c2d71565067c
JeffreyThompson <jeffathomp@gmail.com>
2020-11-22 13:14:01 -0600

    Material-UI added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+        maxWidth: 900,
+    },
+});
 
 const Home = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Your Fashion Career starts now!</h1>
+        <div className={classes.root}>
+            <Typography variant='h1'>Your Fashion Career starts now!</Typography>
         </div>
     );
 };
