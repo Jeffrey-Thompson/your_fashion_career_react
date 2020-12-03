0fa78f27af7797e51e5b4b1f5b4dcf185ca103f2
JeffreyThompson <jeffathomp@gmail.com>
2020-12-03 11:59:08 -0600

    adjust rating page

--- src/pages/Rate.js
+++ src/pages/Rate.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
         background: theme.palette.primary.main
     },
     inputs: {
-        width: '95%'
+        width: '100%'
     }
 }));
 
@@ -99,16 +99,14 @@ const marks = [
         label: 'Very Poor'
     },
     {
-        value: 2,
-        label: 'Poor'
+        value: 2
     },
     {
         value: 3,
         label: 'Average'
     },
     {
-        value: 4,
-        label: 'Good'
+        value: 4
     },
     {
         value: 5,
@@ -131,13 +129,9 @@ const Rate = () => {
             <Typography variant='h2' className={classes.subheading}>Rate a Fashion Company</Typography>
             <form>
                 <Grid container>
-                    <Grid item xs={4}>
+                    <Grid item xs={12}>
                         <Typography variant='h6' className={classes.subheading}>Company</Typography>
-                        <TextField id='name' name='name' variant='filled' className={classes.inputs}/>
-                        <Typography variant='h6' className={classes.subheading}>Overall Rating</Typography>
-                        <div className={classes.stars}>
-                            <Rating name='overall' precision={0.5} />
-                        </div>
+                        <TextField id='name' name='name' variant='outlined' className={classes.inputs}/>
                         <Typography variant='h6' className={classes.subheading}>Are you a current or former employee?</Typography>
                         <FormControl component='fieldset'>
                             <RadioGroup name='employee' value={employee} onChange={handleEmployee} className={classes.radio}>
@@ -146,64 +140,64 @@ const Rate = () => {
                             </RadioGroup>
                         </FormControl>
                         <Typography variant='h6' className={classes.subheading}>Job Title</Typography>
-                        <TextField id='title' name='title' variant='filled' className={classes.inputs}/>
+                        <TextField id='title' name='title' variant='outlined' className={classes.inputs}/>
                     </Grid>
-                    <Grid item xs={8}>
+                    <Grid item xs={12}>
                         <Typography variant='h6' className={classes.subheading}>Categories</Typography>
                         <Box className={classes.categories}>
                             <Grid container >
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={4} >
-                                    <Typography variant='subtitle2' gutterBottom>Career development</Typography>
+                                    <Typography variant='h6' gutterBottom>Career development</Typography>
                                     <IOSSlider name='development' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3}  />
                                 </Grid>
                                 <Grid item xs={2}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Growth opportunities</Typography>
+                                    <Typography variant='h6' gutterBottom>Growth opportunities</Typography>
                                     <IOSSlider name='growth' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Work-life balance</Typography>
+                                    <Typography variant='h6' gutterBottom>Work-life balance</Typography>
                                     <IOSSlider name='workLife' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3}  />
                                 </Grid>
                                 <Grid item xs={2}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Diversity of workforce</Typography>
+                                    <Typography variant='h6' gutterBottom>Diversity of workforce</Typography>
                                     <IOSSlider name='diversity' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3}  />
                                 </Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Salary & benefits</Typography>
+                                    <Typography variant='h6' gutterBottom>Salary & benefits</Typography>
                                     <IOSSlider name='salary' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={2}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Authenticity</Typography>
+                                    <Typography variant='h6' gutterBottom>Authenticity</Typography>
                                     <IOSSlider name='authenticity' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Sustainable business practice</Typography>
+                                    <Typography variant='h6' gutterBottom>Sustainable business practice</Typography>
                                     <IOSSlider name='sustainability' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={2}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Management</Typography>
+                                    <Typography variant='h6' gutterBottom>Management</Typography>
                                     <IOSSlider name='management' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={1}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Kindness</Typography>
+                                    <Typography variant='h6' gutterBottom>Kindness</Typography>
                                     <IOSSlider name='treatment' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={2}></Grid>
                                 <Grid item xs={4}>
-                                    <Typography variant='subtitle2' gutterBottom>Creative Freedom</Typography>
+                                    <Typography variant='h6' gutterBottom>Creative Freedom</Typography>
                                     <IOSSlider name='freedom' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
                                 </Grid>
                                 <Grid item xs={1}></Grid>
@@ -212,13 +206,13 @@ const Rate = () => {
                     </Grid>
                     <Grid item xs={12}>
                         <Typography variant='h6' className={classes.subheading}>Review title</Typography>
-                        <TextField id='reviewTitle' name='reviewTitle' variant='filled' className={classes.inputs}/>
+                        <TextField id='reviewTitle' name='reviewTitle' variant='outlined' className={classes.inputs}/>
                         <Typography variant='h6' className={classes.subheading}>General comments</Typography>
-                        <TextField id='comments' name='comments' variant='filled' multiline rows={5} className={classes.inputs}/>
+                        <TextField id='comments' name='comments' variant='outlined' multiline rows={5} className={classes.inputs}/>
                     </Grid>
                 <FormControlLabel control={<Checkbox name="agree" required/>} label="I agree to Your Fashion Career's Terms of Use. I certify that this review is a truthful account of my experience with this company." />
-                <Button type='submit' variant='contained' className={classes.button}>Submit</Button>
                 </Grid>
+                <Button type='submit' variant='contained' className={classes.button}>Submit</Button>
             </form>
         </Box>
     )
