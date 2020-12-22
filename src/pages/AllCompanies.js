262259aeae36bc670f533124687377c9881eb569
JeffreyThompson <jeffathomp@gmail.com>
2020-12-22 16:12:05 -0600

    set up company model and hook

--- src/pages/AllCompanies.js
+++ src/pages/AllCompanies.js
@@ -1,6 +1,9 @@
 import React from "react";
+import useCompanies from "../hooks/useCompanies";
 
-const AllCompanies = () => {
+const AllCompanies = (props) => {
+    const [companies, fetchCompanies] = useCompanies();
+    
     return (
         <h1>All Companies Page</h1>
     )
