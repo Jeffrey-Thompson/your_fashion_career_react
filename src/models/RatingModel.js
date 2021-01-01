ae49d73ecd78364fbac13b73a6b359aa551d1756
JeffreyThompson <jeffathomp@gmail.com>
2021-01-01 15:19:47 -0600

    can add review to database

---
+++ src/models/RatingModel.js
@@ -0,0 +1,16 @@
+const URL = 'http://localhost:3001/companies';
+
+class RatingModel {
+    static create = (companyId, review) => {
+        return fetch(`${URL}/${companyId}/reviews`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(review)
+        })
+        .then(response => response.json());
+    }
+}
+
+export default RatingModel;
--- src/pages/Rate.js
+++ src/pages/Rate.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Typography, Box, TextField, Button, Radio, RadioGroup, FormControl, FormControlLabel, Grid, Slider, Checkbox } from '@material-ui/core';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import useCompanies from '../hooks/useCompanies';
+import RatingModel from '../models/RatingModel';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -124,16 +126,135 @@ const marks = [
     }
 ]
 
+const initialValues = {
+    employee: 'current',
+    development: 3,
+    workLife: 3,
+    salary: 3,
+    sustainability: 3,
+    treatment: 3,
+    authenticity: 3,
+    diversity: 3,
+    freedom: 3,
+    growth: 3,
+    management: 3,
+    title: '',
+    reviewTitle: '',
+    text: ''
+}
+
 
 const Rate = (props) => {
     const classes = useStyles();
     
+    const [values, setValues] = useState(initialValues);
     const [company] = useCompanies(props.match.params.id);
-    const [employee, setEmployee] = React.useState('current');
-    
-    const handleEmployee = (event) => {
-        setEmployee(event.target.value);
-    };
+    // const [employee, setEmployee] = React.useState('current');
+    // const [development, setDevelopment] = React.useState(3);
+    // const [workLife, setWorkLife] = React.useState(3);
+    // const [salary, setSalary] = React.useState(3);
+    // const [sustainability, setSustainabilty] = React.useState(3);
+    // const [treatment, setTreatment] = React.useState(3);
+    // const [authenticity, setAuthenticity] = React.useState(3);
+    // const [diversity, setDiversity] = React.useState(3);
+    // const [freedom, setFreedom] = React.useState(3);
+    // const [management, setManagement] = React.useState(3);
+    // const [growth, setGrowth] = React.useState(3);
+    // const [title, setTitle] = React.useState('');
+    // const [reviewTitle, setReviewTitle] = React.useState('');
+    // const [text, setText] = React.useState('');
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    const handleDevelopment = (e, value) => {
+        setValues({
+            ...values,
+            development: value    
+        })
+    }
+
+    const handleWorkLife = (e, value) => {
+        setValues({
+            ...values,
+            workLife: value    
+        })
+    }
+
+    const handleGrowth = (e, value) => {
+        setValues({
+            ...values,
+            growth: value    
+        })
+    }
+
+    const handleDiversity = (e, value) => {
+        setValues({
+            ...values,
+            diversity: value    
+        })
+    }
+
+    const handleSalary = (e, value) => {
+        setValues({
+            ...values,
+            salary: value    
+        })
+    }
+
+    const handleAuthenticity = (e, value) => {
+        setValues({
+            ...values,
+            authenticity: value    
+        })
+    }
+
+    const handleSustainablity = (e, value) => {
+        setValues({
+            ...values,
+            sustainability: value    
+        })
+    }
+
+    const handleManagement = (e, value) => {
+        setValues({
+            ...values,
+            management: value    
+        })
+    }
+
+    const handleTreatment = (e, value) => {
+        setValues({
+            ...values,
+            treatment: value    
+        })
+    }
+
+    const handleFreedom = (e, value) => {
+        setValues({
+            ...values,
+            freedom: value    
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const review = {
+            ...values,
+            company: props.match.params.id
+        }
+
+        RatingModel.create(props.match.params.id, review).then(
+            (data) => {
+                props.history.push(`/companies/${props.match.params.id}`);
+            }
+        );
+    }
 
     return(
         <Box className={classes.root} >
@@ -141,7 +262,7 @@ const Rate = (props) => {
                 <img src={company.logo} alt={company.name} className={classes.logo}></img>
             </Box>
             <Typography variant='h2' className={classes.subheading}>Review {company.name}</Typography>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Grid container>
                     <Grid item xs={12}>
                         <Box className={classes.categories} >
@@ -149,60 +270,60 @@ const Rate = (props) => {
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Career development</Typography>
-                                    <IOSSlider name='development' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3}  />
+                                    <IOSSlider id='development' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.development} onChange={handleDevelopment} />
                                 </Grid>
                                 <Grid item sm={2} ></Grid>
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Growth opportunities</Typography>
-                                    <IOSSlider name='growth' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='growth' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.growth} onChange={handleGrowth}/>
                                 </Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Work-life balance</Typography>
-                                    <IOSSlider name='workLife' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3}  />
+                                    <IOSSlider name='workLife' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.workLife} onChange={handleWorkLife}/>
                                 </Grid>
                                 <Grid item sm={2} ></Grid>
                                 
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Diversity of workforce</Typography>
-                                    <IOSSlider name='diversity' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3}  />
+                                    <IOSSlider name='diversity' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.diversity} onChange={handleDiversity}/>
                                 </Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Salary & benefits</Typography>
-                                    <IOSSlider name='salary' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='salary' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.salary} onChange={handleSalary}/>
                                 </Grid>
                                 <Grid item sm={2} ></Grid>
                                 
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Authenticity</Typography>
-                                    <IOSSlider name='authenticity' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='authenticity' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.authenticity} onChange={handleAuthenticity}/>
                                 </Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={4} xs={12} >
                                     <Typography variant='h6' gutterBottom>Sustainable business practice</Typography>
-                                    <IOSSlider name='sustainability' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='sustainability' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.sustainability} onChange={handleSustainablity}/>
                                 </Grid>
                                 <Grid item sm={2} ></Grid>
                                 
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Management</Typography>
-                                    <IOSSlider name='management' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='management' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.management} onChange={handleManagement}/>
                                 </Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={1}></Grid>
                                 <Grid item sm={4} xs={12}>
                                     <Typography variant='h6' gutterBottom>Kindness</Typography>
-                                    <IOSSlider name='treatment' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='treatment' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.treatment} onChange={handleTreatment}/>
                                 </Grid>
                                 <Grid item sm={2}></Grid>
                                 
                                 <Grid item sm={4} xs={12} >
                                     <Typography variant='h6' gutterBottom>Creative Freedom</Typography>
-                                    <IOSSlider name='freedom' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={3} />
+                                    <IOSSlider name='freedom' aria-label="ios slider" step={1} marks={marks} min={1} max={5} defaultValue={values.freedom} onChange={handleFreedom}/>
                                 </Grid>
                                 <Grid item sm={1}></Grid>
                             </Grid>
@@ -211,22 +332,22 @@ const Rate = (props) => {
                     <Grid item xs={12} container>
                         <Grid item sm={4} xs={6}>
                             <Typography variant='h6' className={classes.subheading}>Review title</Typography>
-                            <TextField id='reviewTitle' name='reviewTitle' variant='outlined' className={classes.inputs}/>
+                            <TextField id='reviewTitle' name='reviewTitle' variant='outlined' className={classes.inputs} value={values.reviewTitle} onChange={handleInputChange}/>
                         </Grid>
                         <Grid item sm={1}></Grid>
                         <Grid item sm={4} xs={6}>
                             <Typography variant='h6' className={classes.subheading}>Job Title</Typography>
-                            <TextField id='title' name='title' variant='outlined' className={classes.inputs}/>
+                            <TextField id='title' name='title' variant='outlined' className={classes.inputs} value={values.title} onChange={handleInputChange}/>
                         </Grid>
                     </Grid>
                     <Grid item xs={12}>
                         <Typography variant='h6' className={classes.subheading}>General comments</Typography>
-                        <TextField id='comments' name='comments' variant='outlined' multiline rows={5} className={classes.inputs}/>
+                        <TextField id='comments' name='text' variant='outlined' multiline rows={5} className={classes.inputs} value={values.text} onChange={handleInputChange}/>
                     </Grid>
                     <Grid item xs={12}>
                         <Typography variant='h6' className={classes.subheading}>Are you a current or former employee?</Typography>
                         <FormControl component='fieldset'>
-                            <RadioGroup name='employee' value={employee} onChange={handleEmployee} className={classes.radio}>
+                            <RadioGroup name='employee' value={values.employee} onChange={handleInputChange} className={classes.radio}>
                                 <FormControlLabel value='current' control={<Radio />} label='Current' />
                                 <FormControlLabel value='former' control={<Radio />} label='Former' />
                             </RadioGroup>
