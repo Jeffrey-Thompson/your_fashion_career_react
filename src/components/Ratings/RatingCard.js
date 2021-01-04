0c7b2e23bb3ce6cff90cb9812a399f0b99a973ab
JeffreyThompson <jeffathomp@gmail.com>
2021-01-04 14:51:42 -0600

    further rating card set up

--- src/components/Ratings/RatingCard.js
+++ src/components/Ratings/RatingCard.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Typography, Card } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        color: theme.palette.text.primary
+    }
+}));
 
 function RatingCard(props) {
-    console.log(props.review)
+    const classes = useStyles();
+    const { reviewTitle, text, overall } = props.review;
+
     return (
-        <div>
-            <h1>{props.review.text}</h1>
-        </div>
+        <Card className={classes.root}>
+            <Typography variant='h3'>{reviewTitle}</Typography>
+        </Card>
     )
 }
     
